5f71990888ee7673b4be6ad77edb96760d778638
jonas <jonas.stasiak@hotmail.com>
2017-08-25 20:37:31 -0300

    Descrição: Inclusão de Ferramentas iniciais para desenvolvimento Projeto Farg.

---
+++ Farg/public/javascripts/layout.js
@@ -0,0 +1,5 @@
+﻿angular.module("LayoutApp", []);
+
+angular.module("LayoutApp").controller("HelloCtrl", function ($scope) {
+    $scope.message = "Olá";
+});
---
+++ Farg/routes/layout.js
@@ -0,0 +1,39 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('layout', { title: 'Express' });
+});
+
+module.exports = router;
+
+angular.module('todoApp', [])
+    .controller('TodoListController', function () {
+        var todoList = this;
+        todoList.todos = [
+            { text: 'learn AngularJS', done: true },
+            { text: 'build an AngularJS app', done: false }];
+
+        todoList.addTodo = function () {
+            todoList.todos.push({ text: todoList.todoText, done: false });
+            todoList.todoText = '';
+        };
+
+        todoList.remaining = function () {
+            var count = 0;
+            angular.forEach(todoList.todos, function (todo) {
+                count += todo.done ? 0 : 1;
+            });
+            return count;
+        };
+
+        todoList.archive = function () {
+            var oldTodos = todoList.todos;
+            todoList.todos = [];
+            angular.forEach(oldTodos, function (todo) {
+                if (!todo.done) todoList.todos.push(todo);
+            });
+        };
+    });
